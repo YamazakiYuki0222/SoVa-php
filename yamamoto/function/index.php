76099d951a829268bf3ba89c4739eed667b91f3c
yamamoto <f.yuya.yamamoto@gmail.com>
2022-12-01 17:11:25 +0900

    function

---
+++ yamamoto/function/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$string = "hello";
+
+
+$ret = strtoupper($string);
+$ret2 = str_shuffle($ret);
+$ret3 = explode('O' , $ret2);
+
+
+if (empty($_POST['url'])) {
+   header('Location: http://127.0.0.1:8080/function/form.php?error=1');
+   exit;
+}
+$url = $_POST['url'];
+$ret4 = file_get_contents($url);
+$param = explode('<title>',$ret4);
+$param2 = explode('</title>',$param[1]);
+
+
+
+var_dump($param2[0]);
+?>
